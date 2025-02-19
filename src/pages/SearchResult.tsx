26208e876396bad7c8cfe3ac9add9d12cdf3ad8b
zafergunayonalan <zafergunay7@gmail.com>
2025-02-19 23:44:20 +0300

    searchResult-created

---
+++ src/pages/SearchResult.tsx
@@ -0,0 +1,34 @@
+import { RootState } from '@reduxjs/toolkit/query'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import Loading from '../components/Loading'
+import BookList from '../components/BookList'
+
+const SearchResult: React.FC = () => {
+    const { searchResults, loading, error } = useSelector((state:RootState) => state.books)
+
+    useEffect(() => {
+        window.scrollTo({top: 650, behavior: "smooth"})
+    })
+
+  return (
+    <div className='home-section'>
+        <div className='home-container'>
+            <div className='books'>
+                <div className='home-title'>
+                    <h3>Search Results</h3>
+                </div>
+                { loading ? (
+                    <Loading/>
+                ): error ? (
+                    <div className='error-message'>{error}</div>
+                ): (
+                    <BookList books={searchResults.slice(0, 16)}/>
+                )}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SearchResult
