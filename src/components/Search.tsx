0a9bd5e907e13610f8f0340bf05eaf442a924f9a
zafergunayonalan <zafergunay7@gmail.com>
2025-02-19 23:35:46 +0300

    search-created

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/search.css';
 import { FaSearch } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import { AppDispatch } from '../redux/store';
+import { useDispatch } from 'react-redux';
+import { fetchSearchResult } from '../redux/bookSlice';
 
 const Search:React.FC = () => {
+
+    const [query, setQuery] = useState("")
+    const navigate = useNavigate()
+    const dispatch:AppDispatch = useDispatch()
+
+    const handleSearch = (e: React.FormEvent) => {
+        e.preventDefault()
+        if (query.trim()) {
+            dispatch(fetchSearchResult(query))
+            navigate("/search")
+            setQuery("")
+        }
+    }
+
   return (
     <div className='search-section'>
         <div className='search-container'>
-            <form>
+            <form onSubmit={handleSearch}>
                 <div className='form-container'>
-                    <input type="text" placeholder='Search for books...'/>
+                    <input type="text" placeholder='Search for books...' value={query} onChange={(e) => setQuery(e.target.value)}/>
                     <button type='submit'>
                         <FaSearch/>
                     </button>
